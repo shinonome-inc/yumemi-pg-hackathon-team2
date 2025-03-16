e7ad638f78eafeaecfed6c1cc0cc3c3f5b19d2b6
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2025-03-16 11:52:48 +0900

    model作成

--- server/app/models.py
+++ server/app/models.py
@@ -9,5 +9,17 @@ class User(db.Model):
     received_favor_count = db.Column(db.Integer, default=0, nullable=False)
     repaid_favor_count = db.Column(db.Integer, default=0, nullable=False)
 
-    def __repr__(self):
-        return f"<User {self.user_name}, Received: {self.received_favor_count}, Repaid: {self.repaid_favor_count}>"
+
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    received_favor_text = db.Column(db.String(255), nullable=True)
+    received_favor_date = db.Column(db.DateTime, nullable=True)
+    giver_name = db.Column(db.String(255), nullable=True)
+    repaid_favor_text = db.Column(db.String(255), nullable=True)
+    repaid_favor_date = db.Column(db.DateTime, nullable=True)
+    memo = db.Column(db.String(255), nullable=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=db.func.now(), onupdate=db.func.now()
+    )
