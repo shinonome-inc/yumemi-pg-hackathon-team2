9bc08feab09bc39162ad50273303bae788a87380
wtrinoue <w.inou.24.8.1574@4nonome.com>
2025-03-20 23:08:02 +0900

    受恩と報恩のリストの改修

--- app/lib/page/home/received_favor_list.dart
+++ app/lib/page/home/received_favor_list.dart
@@ -16,27 +16,112 @@ class ReceivedFavorList extends ConsumerWidget {
         Expanded(
           child: receivedFavors.when(
             data: (favors) => Scaffold(
+              backgroundColor: Colors.orange.withValues(alpha: 0.05),
               body: ListView.builder(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                padding: EdgeInsets.symmetric(horizontal: 7, vertical: 7),
                 itemCount: favors.length,
                 itemBuilder: (context, index) {
                   final favor = favors[index];
-                  return Card(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5)),
-                    elevation: 8,
-                    child: ListTile(
-                      title: Text(favor.favorText),
-                      subtitle: Text("by ${favor.giverName}"),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => FavorDetailPage(favor: favor),
+                  final favorDateString =
+                      "${favor.favorDate.year}/${favor.favorDate.month}/${favor.favorDate.day}";
+                  return Dismissible(
+                    key: Key(favors[index].toString()),
+                    direction: DismissDirection.endToStart,
+                    background: Container(
+                      color: Colors.greenAccent,
+                      child: const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Icon(Icons.check)),
+                    ),
+                    secondaryBackground: Container(
+                      color: Colors.red,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "削除",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
                           ),
-                        );
-                      },
+                          SizedBox(
+                            width: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                    onDismissed: (DismissDirection direction) {
+                      if (direction == DismissDirection.endToStart) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text("削除されました。")));
+                        favors.removeAt(index);
+                      }
+                    },
+                    child: Card(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                      elevation: 4,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.account_circle,
+                          color: Colors.blue,
+                          size: 50,
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Stack(
+                              children: [
+                                Text(
+                                  favor.giverName,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Color(0xFF7C7C7C),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                    favorDateString,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color:
+                                          Colors.black.withValues(alpha: 0.3),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              favor.favorText,
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              favor.memo,
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Color(0xFFBCBCBC),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  FavorDetailPage(favor: favor),
+                            ),
+                          );
+                        },
+                      ),
                     ),
                   );
                 },
--- app/lib/page/home/repaid_favor_list.dart
+++ app/lib/page/home/repaid_favor_list.dart
@@ -15,41 +15,104 @@ class RepaidFavorList extends ConsumerWidget {
     return receivedFavors.when(
       data: (receivedData) {
         return repaidFavors.when(
-          data: (repaidData) => ListView.builder(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            itemCount: repaidData.length,
-            itemBuilder: (context, index) {
-              final favor = repaidData[index];
-              final linkedReceivedFavor = receivedData.firstWhere(
-                (rf) => rf.id == favor.receivedFavorId,
-                orElse: () => ReceivedFavor(
-                  id: '',
-                  giverName: '不明',
-                  favorText: '関連する恩なし',
-                  favorDate: DateTime.now(),
-                ),
-              );
-
-              return Card(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-                child: ListTile(
-                  title: Text(favor.favorText),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("奉公日: ${favor.favorDate.toLocal()}"),
-                      const SizedBox(height: 8),
-                      Text("【紐づく御恩】"),
-                      Text("恩人: ${linkedReceivedFavor.giverName}"),
-                      Text("内容: ${linkedReceivedFavor.favorText}"),
-                      Text("受けた日: ${linkedReceivedFavor.favorDate.toLocal()}"),
-                    ],
+          data: (repaidData) => Scaffold(
+            backgroundColor: Colors.blue.withValues(alpha: 0.05),
+            body: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              itemCount: repaidData.length,
+              itemBuilder: (context, index) {
+                final favor = repaidData[index];
+                final repaidFavorDateString =
+                    "${favor.favorDate.year}/${favor.favorDate.month}/${favor.favorDate.day}";
+                final linkedReceivedFavor = receivedData.firstWhere(
+                  (rf) => rf.id == favor.receivedFavorId,
+                  orElse: () => ReceivedFavor(
+                    id: '',
+                    giverName: '不明',
+                    favorText: '関連する恩なし',
+                    favorDate: DateTime.now(),
                   ),
-                ),
-              );
-            },
+                );
+                final receivedFavorDateString =
+                    "${linkedReceivedFavor.favorDate.year}/${linkedReceivedFavor.favorDate.month}/${linkedReceivedFavor.favorDate.day}";
+
+                // return Card(
+                //   color: Colors.white,
+                //   shape: RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.circular(5)),
+                //   child: ListTile(
+                //     title: Text(favor.favorText),
+                //     subtitle: Column(
+                //       crossAxisAlignment: CrossAxisAlignment.start,
+                //       children: [
+                //         Text("奉公日: ${favor.favorDate.toLocal()}"),
+                //         const SizedBox(height: 8),
+                //         Text("【紐づく御恩】"),
+                //         Text("恩人: ${linkedReceivedFavor.giverName}"),
+                //         Text("内容: ${linkedReceivedFavor.favorText}"),
+                //         Text("受けた日: ${linkedReceivedFavor.favorDate.toLocal()}"),
+                //       ],
+                //     ),
+                //   ),
+                // );
+                return Card(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  elevation: 0,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.account_circle,
+                      color: Colors.blue,
+                      size: 50,
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Stack(
+                          children: [
+                            Text(
+                              linkedReceivedFavor.giverName,
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Color(0xFF7C7C7C),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                repaidFavorDateString,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.black.withValues(alpha: 0.3),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          favor.favorText,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Color(0xFFBCBCBC),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          favor.memo + "⇨" + repaidFavorDateString,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
           loading: () => const Center(child: CircularProgressIndicator()),
           error: (error, _) => Center(child: Text('エラー: $error')),
