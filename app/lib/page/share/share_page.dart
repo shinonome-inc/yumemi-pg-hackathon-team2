34195657de000f6064deedeaaef779f681ab3d73
wtrinoue <w.inou.24.8.1574@4nonome.com>
2025-03-23 23:05:11 +0900

    みんなの恩ページの作成

--- app/lib/page/share/share_page.dart
+++ app/lib/page/share/share_page.dart
@@ -1,36 +1,82 @@
-import 'package:app/provider/share_notifier.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:riverpod_paging_utils/riverpod_paging_utils.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'all_favor_list.dart';
+import 'ranking_favor_list.dart';
 
-class SharePage extends StatelessWidget {
+final StateProvider<int> allRankProvider = StateProvider((ref) {
+  return 0;
+});
+
+class SharePage extends ConsumerWidget {
   const SharePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('みんなの恩返し'),
-      ),
-      body: PagingHelperView(
-        provider: shareNotifierProvider,
-        futureRefreshable: shareNotifierProvider.future,
-        notifierRefreshable: shareNotifierProvider.notifier,
-        contentBuilder: (data, widgetCount, endItemView) => ListView.builder(
-          itemCount: widgetCount,
-          itemBuilder: (context, index) {
-            if (index == widgetCount - 1) {
-              return endItemView;
-            }
-            final item = data.items[index];
-            final formattedDate =
-                DateFormat('yyyy/MM/dd HH:mm').format(item.repaiedFavorDate);
-            return ListTile(
-              title: Text(
-                  '${item.userId}さんが${item.receivedFavorText}のに対して${item.repaidFavorText}'),
-              subtitle: Text(formattedDate),
-            );
-          },
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int topBarIndex = ref.watch(allRankProvider);
+    final Color topBarColor = [Colors.orange, Colors.blue][topBarIndex];
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Stack(
+            children: [
+              Column(children: [
+                SizedBox(
+                  height: 15,
+                ),
+                Center(
+                  child: Text(
+                    "みんなの恩返し",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.notifications_none,
+                      size: 30,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.settings_outlined,
+                      size: 30,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'みんなの恩'),
+              Tab(text: 'ランキング'),
+            ],
+            labelColor: topBarColor,
+            indicatorColor: topBarColor,
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(width: 4.0, color: topBarColor),
+              insets: EdgeInsets.symmetric(horizontal: 90),
+            ),
+            onTap: (index) {
+              ref.read(allRankProvider.notifier).state = index;
+            },
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            allFavorList(),
+            rankingFavorList(),
+          ],
         ),
       ),
     );
